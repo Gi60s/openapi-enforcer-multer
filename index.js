3b955f1ab34d5864152812b9349a0fd080a2ca28
james <james.speirs@gmail.com>
2019-05-07 09:39:58 -0600

    works with multer memory or file system as well as oas v2 and v3

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
-
 const methods = ['delete', 'get', 'head', 'options', 'patch', 'post', 'put', 'trace']
 
-module.exports = function (enforcer, upload, { extension = 'x-multer' } = {}) {
+module.exports = function (enforcer, upload) {
   const map = new WeakMap()
 
   const promise = enforcer.promise
     .then(openapi => {
       if (openapi.paths) {
         Object.keys(openapi.paths).forEach(pathKey => {
-          methods.forEach(method => {
-            const path = openapi.paths[pathKey]
-            if (path) {
-              methods.forEach(method => {
-                const operation = path[method]
-                if (operation) {
-
-
-                  // v2
-                  const v2Body = operation.parameters.find(p => p.in === 'formData')
-                  if (v2Body) {
-                    // TODO: implement
-                    throw Error('Not yet implemented')
-
-                    // v3
-                  } else if (operation.requestBody) {
-                    const schema = operation.requestBody.content &&
-                      operation.requestBody.content['multipart/form-data'] &&
-                      operation.requestBody.content['multipart/form-data'].schema
-                    if (schema && schema.type === 'object' && schema.properties) {
-                      const fields = []
-                      Object.keys(schema.properties).forEach(key => {
-                        const item = schema.properties[key]
-                        if (item.type === 'array' && item.items && schemaIsFileType(item.items)) {
-                          const config = { name: key }
-                          if (item.hasOwnProperty('maxItems')) config.maxCount = item.maxItems
-                          fields.push(config)
-                        } else if (schemaIsFileType(item)) {
-                          fields.push({ name: key, maxCount: 1 })
-                        }
+          const path = openapi.paths[pathKey]
+          if (path) {
+            methods.forEach(method => {
+              const operation = path[method]
+              if (operation) {
+                // v2
+                const v2Body = operation.parameters.find(p => p.in === 'formData')
+                if (v2Body) {
+                  const { definition, root } = operation.enforcerData
+                  const consumes = (definition.consumes || []).concat(root.definition.consumes || [])
+                  if (consumes.indexOf('multipart/form-data') !== -1) {
+                    const schema = { type: 'object', properties: {} }
+                    operation.allParameters
+                      .filter(param => param.in === 'formData')
+                      .forEach(param => {
+                        schema.properties[param.name] = param.schema
                       })
-                      if (fields.length) {
-                        operation.multer = {
-                          middleware: upload.fields(fields),
-                          schema
-                        }
-                      }
-                    }
+                    buildMulterFields(schema, operation, map, upload)
                   }
+
+                // v3
+                } else if (operation.requestBody) {
+                  const schema = operation.requestBody.content &&
+                    operation.requestBody.content['multipart/form-data'] &&
+                    operation.requestBody.content['multipart/form-data'].schema
+                  buildMulterFields(schema, operation, map, upload)
                 }
-              })
-            }
-          })
+              }
+            })
+          }
         })
       }
       return openapi
@@ -63,7 +49,7 @@ module.exports = function (enforcer, upload, { extension = 'x-multer' } = {}) {
         // get the x-multer property off the operation instance for the request
         const [ path ] = openapi.path(req.method, req.path)
         const operation = path.operation
-        const multer = operation.multer
+        const multer = map.get(operation)
 
         // if there is no multer middleware
         if (!multer) {
@@ -82,9 +68,11 @@ module.exports = function (enforcer, upload, { extension = 'x-multer' } = {}) {
               const files = req.files[key]
               const prop = multer.schema.properties[key]
               if (prop.type === 'array') {
-                req.body[key] = files.map(file => file.buffer)
+                req.body[key] = files.map(file => file.buffer || Buffer.allocUnsafe(file.size))
               } else if (files.length) {
-                req.body[key] = files[files.length - 1].buffer
+                const file = files[files.length - 1]
+                req.body[key] = file.buffer || Buffer.allocUnsafe(file.size)
+                req.files[key] = file
               }
             })
             next()
@@ -92,6 +80,28 @@ module.exports = function (enforcer, upload, { extension = 'x-multer' } = {}) {
         }
       })
       .catch(next)
+  }
+}
+
+function buildMulterFields (schema, operation, map, upload) {
+  if (schema && schema.type === 'object' && schema.properties) {
+    const fields = []
+    Object.keys(schema.properties).forEach(key => {
+      const item = schema.properties[key]
+      if (item.type === 'array' && item.items && schemaIsFileType(item.items)) {
+        const config = { name: key }
+        if (item.hasOwnProperty('maxItems')) config.maxCount = item.maxItems
+        fields.push(config)
+      } else if (schemaIsFileType(item)) {
+        fields.push({ name: key, maxCount: 1 })
+      }
+    })
+    if (fields.length) {
+      map.set(operation, {
+        middleware: upload.fields(fields),
+        schema
+      })
+    }
   }
 }
 

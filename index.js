7fd97998a719d7654d86d9c496d0c3e117859530
James Speirs <Gi60s@users.noreply.github.com>
2019-12-23 09:21:08 -0700

    Update index.js
    Update spacing to use two spaces instead of 4 (maintain indentation style)

--- index.js
+++ index.js
@@ -1,127 +1,127 @@
 const methods = ['delete', 'get', 'head', 'options', 'patch', 'post', 'put', 'trace']
 
 module.exports = function (enforcer, upload, options) {
-    const map = new WeakMap()
+  const map = new WeakMap()
 
-    if (!options) options = {}
-    if (!options.hasOwnProperty('directedUploads')) options.directedUploads = false
+  if (!options) options = {}
+  if (!options.hasOwnProperty('directedUploads')) options.directedUploads = false
 
-    const promise = enforcer.promise
-        .then(openapi => {
-            if (openapi.paths) {
-                Object.keys(openapi.paths).forEach(pathKey => {
-                    const path = openapi.paths[pathKey]
-                    if (path) {
-                        methods.forEach(method => {
-                            const operation = path[method]
-                            if (operation) {
-                                // v2
-                                const v2Body = operation.parameters.find(p => p.in === 'formData')
-                                if (v2Body) {
-                                    const {definition, root} = operation.enforcerData
-                                    const consumes = (definition.consumes || []).concat(root.definition.consumes || [])
-                                    if (consumes.indexOf('multipart/form-data') !== -1) {
-                                        const schema = {type: 'object', properties: {}}
-                                        operation.allParameters
-                                            .filter(param => param.in === 'formData')
-                                            .forEach(param => {
-                                                schema.properties[param.name] = param.schema
-                                            })
-                                        buildMulterFields(schema, operation, map, upload, options)
-                                    }
+  const promise = enforcer.promise
+    .then(openapi => {
+      if (openapi.paths) {
+        Object.keys(openapi.paths).forEach(pathKey => {
+          const path = openapi.paths[pathKey]
+          if (path) {
+            methods.forEach(method => {
+              const operation = path[method]
+              if (operation) {
+                // v2
+                const v2Body = operation.parameters.find(p => p.in === 'formData')
+                if (v2Body) {
+                  const {definition, root} = operation.enforcerData
+                  const consumes = (definition.consumes || []).concat(root.definition.consumes || [])
+                  if (consumes.indexOf('multipart/form-data') !== -1) {
+                    const schema = {type: 'object', properties: {}}
+                    operation.allParameters
+                      .filter(param => param.in === 'formData')
+                      .forEach(param => {
+                        schema.properties[param.name] = param.schema
+                      })
+                    buildMulterFields(schema, operation, map, upload, options)
+                  }
 
-                                    // v3
-                                } else if (operation.requestBody) {
-                                    const schema = operation.requestBody.content &&
-                                        operation.requestBody.content['multipart/form-data'] &&
-                                        operation.requestBody.content['multipart/form-data'].schema
-                                    buildMulterFields(schema, operation, map, upload, options)
-                                }
-                            }
-                        })
-                    }
-                })
-            }
-            return openapi
-        })
-
-    return function (req, res, next) {
-        promise
-            .then(openapi => {
-
-                // get the x-multer property off the operation instance for the request
-                const [path] = openapi.path(req.method, req.path)
-                const operation = path.operation
-                const multer = map.get(operation)
-
-                // if there is no multer middleware
-                if (!multer) {
-                    next()
-
-                    // there is a multer middleware
-                } else {
-
-                    // run the multer middleware
-                    Object.assign(req.params, path.params)
-                    multer.middleware(req, res, function (err) {
-                        if (err) return next(err)
-
-                        // to correctly apply modification only for form-data type
-                        if (req.is('multipart')) {
-                            // copy multer's "files" to body
-                            req.body = Object.assign({}, req.body)
-                            Object.keys(req.files || []).forEach(key => {
-                                const files = req.files[key]
-                                const prop = multer.schema.properties[key]
-                                if (prop.type === 'array') {
-                                    req.body[key] = files.map(file => file.buffer || Buffer.allocUnsafe(file.size))
-                                } else if (files.length) {
-                                    const file = files[files.length - 1]
-                                    req.body[key] = file.buffer || Buffer.allocUnsafe(file.size)
-                                    req.files[key] = file
-                                }
-                            })
-                        }
-                        next()
-                    })
+                  // v3
+                } else if (operation.requestBody) {
+                  const schema = operation.requestBody.content &&
+                    operation.requestBody.content['multipart/form-data'] &&
+                    operation.requestBody.content['multipart/form-data'].schema
+                  buildMulterFields(schema, operation, map, upload, options)
                 }
+              }
             })
-            .catch(next)
-    }
+          }
+        })
+      }
+      return openapi
+    })
+
+  return function (req, res, next) {
+    promise
+      .then(openapi => {
+
+        // get the x-multer property off the operation instance for the request
+        const [path] = openapi.path(req.method, req.path)
+        const operation = path.operation
+        const multer = map.get(operation)
+
+        // if there is no multer middleware
+        if (!multer) {
+          next()
+
+          // there is a multer middleware
+        } else {
+
+          // run the multer middleware
+          Object.assign(req.params, path.params)
+          multer.middleware(req, res, function (err) {
+            if (err) return next(err)
+
+            // to correctly apply modification only for form-data type
+            if (req.is('multipart')) {
+              // copy multer's "files" to body
+              req.body = Object.assign({}, req.body)
+              Object.keys(req.files || []).forEach(key => {
+                const files = req.files[key]
+                const prop = multer.schema.properties[key]
+                if (prop.type === 'array') {
+                  req.body[key] = files.map(file => file.buffer || Buffer.allocUnsafe(file.size))
+                } else if (files.length) {
+                  const file = files[files.length - 1]
+                  req.body[key] = file.buffer || Buffer.allocUnsafe(file.size)
+                  req.files[key] = file
+                }
+              })
+            }
+            next()
+          })
+        }
+      })
+      .catch(next)
+  }
 }
 
 function buildMulterFields(schema, operation, map, uploadMap, {directedUploads}) {
-    if (schema && schema.type === 'object' && schema.properties) {
-        const fields = []
-        const multerKey = directedUploads
-            ? operation['x-multer-key'] || operation.enforcerData.parent.result['x-multer-key']
-            : ''
-        const upload = multerKey
-            ? uploadMap[multerKey]
-            : uploadMap
+  if (schema && schema.type === 'object' && schema.properties) {
+    const fields = []
+    const multerKey = directedUploads
+      ? operation['x-multer-key'] || operation.enforcerData.parent.result['x-multer-key']
+      : ''
+    const upload = multerKey
+      ? uploadMap[multerKey]
+      : uploadMap
 
-        if (!upload && multerKey) throw Error('Unable to find multer definition with the specified key: ' + multerKey)
-        if (!upload) throw Error('Invalid multer object provided')
+    if (!upload && multerKey) throw Error('Unable to find multer definition with the specified key: ' + multerKey)
+    if (!upload) throw Error('Invalid multer object provided')
 
-        Object.keys(schema.properties).forEach(key => {
-            const item = schema.properties[key]
-            if (item.type === 'array' && item.items && schemaIsFileType(item.items)) {
-                const config = {name: key}
-                if (item.hasOwnProperty('maxItems')) config.maxCount = item.maxItems
-                fields.push(config)
-            } else if (schemaIsFileType(item)) {
-                fields.push({name: key, maxCount: 1})
-            }
-        })
-        if (fields.length) {
-            map.set(operation, {
-                middleware: upload.fields(fields),
-                schema
-            })
-        }
+    Object.keys(schema.properties).forEach(key => {
+      const item = schema.properties[key]
+      if (item.type === 'array' && item.items && schemaIsFileType(item.items)) {
+        const config = {name: key}
+        if (item.hasOwnProperty('maxItems')) config.maxCount = item.maxItems
+        fields.push(config)
+      } else if (schemaIsFileType(item)) {
+        fields.push({name: key, maxCount: 1})
+      }
+    })
+    if (fields.length) {
+      map.set(operation, {
+        middleware: upload.fields(fields),
+        schema
+      })
     }
+  }
 }
 
 function schemaIsFileType(schema) {
-    return schema.type === 'string' && (schema.format === 'byte' || schema.format === 'binary')
+  return schema.type === 'string' && (schema.format === 'byte' || schema.format === 'binary')
 }
